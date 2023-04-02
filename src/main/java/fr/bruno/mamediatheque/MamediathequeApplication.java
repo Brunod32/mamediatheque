69747a3b1a3bdc17848abd149cc55bc728abb4fc
Bruno Dahlem <bruno.dahlem2@gmail.com>
2023-04-02 10:06:44 +0200

    Initialization project

---
+++ src/main/java/fr/bruno/mamediatheque/MamediathequeApplication.java
@@ -0,0 +1,13 @@
+package fr.bruno.mamediatheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamediathequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MamediathequeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/bruno/mamediatheque/MamediathequeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bruno.mamediatheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamediathequeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
